cc1c848265b14dc420295e1a1302d327f86f2ef0
Dominic Rodger <dominicrodger+github@gmail.com>
2013-04-29 17:37:00 +0100

    Use a dummy tracker, rather than actually fetching memory

--- plotfree/tests.py
+++ plotfree/tests.py
@@ -4,29 +4,35 @@ import unittest
 from .memory_tracker import MemoryTracker
 
 
-class TestMemoryTracker(unittest.TestCase):
+class DummyTracker(MemoryTracker):
+    def add_data_point(self):
+        self.data_points.append((1, 2))
+
+
+class TestTracker(unittest.TestCase):
     def test_dump_empty_file(self):
         f = os.tmpnam()
-        writer = MemoryTracker(f)
+        writer = DummyTracker(f)
         writer.dump()
-        reader = MemoryTracker(f)
+        reader = DummyTracker(f)
         self.assertEqual(reader.data_points, [])
 
     def test_dump_single_point_file(self):
         f = os.tmpnam()
-        writer = MemoryTracker(f)
+        writer = DummyTracker(f)
         writer.add_data_point()
         writer.dump()
-        reader = MemoryTracker(f)
+        reader = DummyTracker(f)
         self.assertEqual(len(reader.data_points), 1)
+        self.assertEqual(reader.data_points[0], [1, 2])
 
     def test_dump_limited_data_points(self):
         f = os.tmpnam()
-        writer = MemoryTracker(f)
+        writer = DummyTracker(f)
 
         for i in range(0, 10):
             writer.add_data_point()
         writer.dump(4)
 
-        reader = MemoryTracker(f)
+        reader = DummyTracker(f)
         self.assertEqual(len(reader.data_points), 4)
